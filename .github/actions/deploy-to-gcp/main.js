43b53c472ab5954c25328b3e67a996f229795dd9
orzumurod parmonov <orzumurodparmonov@gmail.com>
2024-10-17 10:46:32 +0500

    debugging tests added

--- .github/actions/deploy-to-gcp/main.js
+++ .github/actions/deploy-to-gcp/main.js
@@ -10,9 +10,24 @@ async function run() {
 
         // Write the service account key to a temporary file
         const serviceAccountKey = process.env.GCP_SERVICE_ACCOUNT_KEY;
+        // Check if the key exists
+        if (!serviceAccountKey) {
+            core.setFailed('Service account key is missing or not set correctly.');
+            process.exit(1);
+        }
+
         const keyFilePath = './gcp-key.json';
         fs.writeFileSync(keyFilePath, serviceAccountKey);
 
+
+        // Verify the contents of the gcp-key.json file
+        fs.readFile(keyFilePath, 'utf8', (err, data) => {
+            if (err) {
+                core.setFailed(`Error reading the file: ${err.message}`);
+            } else {
+                core.info(`Contents of gcp-key.json: ${data}`);
+            }
+        });
         // Authenticate using the service account key
         await exec.exec('gcloud', ['auth', 'activate-service-account', '--key-file', keyFilePath]);
 
